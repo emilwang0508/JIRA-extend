52e3d46ada936c6ac4a06ce8f9ea8886dfdc1c3c
wangyujia <emiloffice@163.com>
2017-11-27 09:37:23 +0800

    modify event api

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -215,7 +215,8 @@ class HomeController extends Controller
         $jql = 'project = SD AND status = "To Do" AND Sprint = '.env('SPRINT_ID').' AND assignee in (EMPTY) order by lastViewed DESC';
         $res = $this->jira($jql);
         $total = $res->total;//获得任务数组
-        if ($total!==''||$total!==0){
+        dd($total!=="");
+        if ($total!==0){
             $text = '<speak>Please volunteer for unassigned task.</speak>';
             $voiceUrl = $this->polly($text);
             $data['voiceUrl'] = $voiceUrl;
